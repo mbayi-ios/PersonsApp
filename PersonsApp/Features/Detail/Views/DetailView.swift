960e2870c20afb2a8e03e2b5c2e0f32c8222f4b6
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-12-20 00:50:57 +0300

    display data on detail views and able to navigate to detail view

--- PersonsApp/Features/Detail/Views/DetailView.swift
+++ PersonsApp/Features/Detail/Views/DetailView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct DetailView: View {
+    @State private var userInfo: UserDetailResponse?
+
     var body: some View {
         ZStack {
             background
 
             ScrollView {
                 VStack(alignment: .leading, spacing: 18) {
+                    avatar
                     Group {
                         general
 
@@ -25,14 +28,23 @@ struct DetailView: View {
                 }
                 .padding()
             }
-
+        }
+        .navigationTitle("Details")
+        .onAppear {
+            do {
+                userInfo = try StaticJSONMapper.decode(file: "SingleUserData", type: UserDetailResponse.self)
+            } catch {
+                print(error)
+            }
         }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
+        NavigationView {
         DetailView()
+    }
     }
 }
 
@@ -41,17 +53,44 @@ private extension DetailView {
         Theme.background
             .ignoresSafeArea( edges: .top)
     }
-    var link: some View {
-        Link(destination: .init(string: "https://www.google.com")!) {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("Support Reqres")
-                    .foregroundColor(Theme.text)
-                    .font(.system(.body, design: .rounded).weight(.semibold))
-                Text("https://www.google.com")
+
+    @ViewBuilder
+    var avatar: some View {
+        if let avatarAbsoluteString = userInfo?.data.avatar,
+           let avatarUrl = URL(string: avatarAbsoluteString) {
+            AsyncImage(url: avatarUrl) { image in
+                image
+                    .resizable()
+                    .aspectRatio( contentMode: .fill)
+                    .frame(height: 250)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
             }
-            Spacer()
-            Symbols.link
-                .font(.system(.title3, design: .rounded))
+            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+        }
+    }
+
+    @ViewBuilder
+    var link: some View {
+        if let supportAbsoluteString = userInfo?.support.url,
+           let supportUrl = URL(string: supportAbsoluteString),
+           let supportText = userInfo?.support.text {
+            Link(destination: supportUrl) {
+                VStack(alignment: .leading, spacing: 8 ) {
+                    Text(supportText)
+                        .foregroundColor(Theme.text)
+                        .font(
+                            .system(.body, design: .rounded)
+                                .weight(.semibold)
+                        )
+                        .multilineTextAlignment(.leading)
+                    Text(supportAbsoluteString)
+                }
+                Spacer()
+                Symbols.link
+                    .font(.system(.title3, design: .rounded))
+            }
         }
     }
 }
@@ -59,7 +98,7 @@ private extension DetailView {
 private extension DetailView {
     var general: some View {
         VStack(alignment: .leading, spacing: 8) {
-            PillView(id: 0)
+            PillView(id: userInfo?.data.id ?? 0)
 
             Group {
                 firstName
@@ -77,31 +116,31 @@ private extension DetailView {
                 .system(.body, design: .rounded)
                     .weight(.semibold)
             )
-        Text("First Name here")
+        Text(userInfo?.data.firstName ?? "-")
             .font(.system(.subheadline, design: .rounded))
         Divider()
     }
 
     @ViewBuilder
     var lastName: some View {
-        Text("First Name")
+        Text("Last Name")
             .font(
                 .system(.body, design: .rounded)
                     .weight(.semibold)
             )
-        Text("First Name here")
+        Text(userInfo?.data.lastName ?? "-")
             .font(.system(.subheadline, design: .rounded))
         Divider()
     }
 
     @ViewBuilder
     var email: some View {
-        Text("First Name")
+        Text("Email Address")
             .font(
                 .system(.body, design: .rounded)
                     .weight(.semibold)
             )
-        Text("First Name here")
+        Text(userInfo?.data.email ?? "-")
             .font(.system(.subheadline, design: .rounded))
         Divider()
     }
