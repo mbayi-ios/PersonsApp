311f136c51ec0cb049419dd80ed6f59ebe8843cb
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-12-19 22:27:46 +0300

    theme setup and symbols

---
+++ PersonsApp/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  PersonsApp
+//
+//  Created by Amby on 19/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    static let person = Image("person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ PersonsApp/Resources/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  PersonsApp
+//
+//  Created by Amby on 19/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
